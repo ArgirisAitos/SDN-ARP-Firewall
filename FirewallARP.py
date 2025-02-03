b0b590dde80454206a0326ff563563bd7da5d596
Argiris Aitos <aitos.ar49@gmail.com>
2025-02-03 19:38:51 +0200

    Create Firewall for ryu-controller

---
+++ FirewallARP.py
@@ -0,0 +1,36 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, arp
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+       
+        self.arp_table = {}
+        self.arp_table = {
+            '10.0.0.1': '52:a2:b2:97:17:c8',
+            '10.0.0.2': '42:96:15:ec:4f:2f',
+            '10.0.0.3': '42:fd:3e:81:6e:3c',
+            '10.0.0.4': '7e:86:c9:34:71:a4'
+        }
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+
+
+        match = parser.OFPMatch(eth_type=0x0806, ip_proto=1)
+        actions = [parser.OFPActionOutput(ofproto.OFPP_NORMAL)]
+        self.add_flow(datapath, 10,match,actions)
