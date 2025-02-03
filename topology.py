43a9a6ec339c5129c4ecaf9490caad773ebdc9ca
Argiris Aitos <aitos.ar49@gmail.com>
2025-02-03 19:33:29 +0200

    Update topology

--- topology.py
+++ topology.py
@@ -1,22 +1,18 @@
-from ryu.base import app_manager
-from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
-from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet, ethernet, arp
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel
 
-class SimpleSwitch13(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+class MyTopo(Topo):
+    def build(self):
+        switch = self.addSwitch('s1')
 
-    def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
-       
-        #a static ARP table to map IP addresses to MAC addresses
-        self.arp_table = {
-            '10.0.0.1': '52:a2:b2:97:17:c8',
-            '10.0.0.2': '42:96:15:ec:4f:2f',
-            '10.0.0.3': '42:fd:3e:81:6e:3c',
-            '10.0.0.4': '7e:86:c9:34:71:a4'
+        hosts = {
+            'h1': ('10.0.0.1', '52:a2:b2:97:17:c8'),
+            'h2': ('10.0.0.2', '42:96:15:ec:4f:2f'),
+            'h3': ('10.0.0.3', '42:fd:3e:81:6e:3c'),
+            'h4': ('10.0.0.4', '46:4f:66:a6:98:72')
         }
 for h, (ip, mac) in hosts.items():
             host = self.addHost(h, ip=ip, mac=mac)
